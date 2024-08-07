5b23858a487f44234c53b94f05a1a23f25b8cf0d
Itsguri31 <gurkirats0001@gmail.com>
2024-08-07 19:08:11 -0400

    Menu Model createdHomepage for showing the different dishs the users order is created

---
+++ Tiffindelight/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+namespace Tiffindelight.ViewModels
+{
+    public partial class HomePageViewModel: ObservableRecipient
+    {
+        public HomePageViewModel() { 
+        
+        }
+    }
+}
